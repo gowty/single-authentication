dae184fa831e54cdaad7853ae31bdf4f2a98f7ae
gowtham <867573@gmail.com>
2018-01-12 15:01:28 +0530

    make it work

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
-var express               = require("express"),
-    app                   = express(),
-    flash                 = require("connect-flash"),
-    mongoose              = require("mongoose"),
-    bodyParser            = require("body-parser"),
-    passport              = require("passport"),
-    LocalStrategy         = require("passport-local"),
-    passportLocalMongoose = require("passport-local-mongoose"),
-    User                  = require("./models/user.js");
+var express = require("express"),
+  app = express(),
+  flash = require("connect-flash"),
+  mongoose = require("mongoose"),
+  methodOverride = require("method-override"),
+  bodyParser = require("body-parser"),
+  passport = require("passport"),
+  LocalStrategy = require("passport-local"),
+  passportLocalMongoose = require("passport-local-mongoose"),
+  User = require("./models/user.js");
 
-mongoose.connect("mongodb://localhost/kitapp");
-
+mongoose.connect("process.env.MONGODB_URI");
+var port = process.env.PORT || 5000;
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(methodOverride("_method"));
+
+// admin login & logout
 app.use(flash());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   require("express-session")({
-    secret: "Gowty did th app",
+    secret: "Gowty did the app",
     resave: false,
     saveUninitialized: false
   })
@@ -36,57 +40,85 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.get("/", function(req, res) {
+app.get("/home", function(req, res) {
   res.render("landing");
 });
 
-app.get("/dashboard",isLoggedIn, function(req, res) {
-  res.render("dashboard");
+app.get("/", isLoggedIn, function(req, res) {
+  res.render("dashboard", { user: req.user });
 });
 
 // Authentication
 // create admin
-app.post("/adminregister", function(req, res) {
-  req.body.username
-  req.body.firstname
-  req.body.lastname
-  req.body.code
-  req.body.password
-
-  User.register(new User({username:req.body.username,firstname:req.body.firstname,lastname:req.body.lastname,code:req.body.code}),req.body.password,function(err,user){
-    if(err){
-      console.log(err);
-      res.redirect("/");
-    }
-      passport.authenticate("local")(req,res,function(){
+app.post("/register", function(req, res) {
+  User.register(
+    new User({
+      username: req.body.username,
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      gender: req.body.gender,
+      mobile: req.body.mobile,
+      address: req.body.address
+    }),
+    req.body.password,
+    function(err, user) {
+      if (err) {
+        console.log(err);
+        res.redirect("/");
+      }
+      passport.authenticate("local")(req, res, function() {
         req.flash("success", "You are Registered and now can LogIn");
         res.redirect("/");
       });
-  });
+    }
+  );
 });
 
 // admin login & logout
-app.post("/adminlogin",passport.authenticate("local",{
-  successRedirect : "/dashboard",
-  failureRedirect : "/"
-}), function(req, res) {
-});
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/home",
+    successFlash: "successfully loggedin",
+    failureFlash: "Incorrect Username or password"
+  })
+);
 
-app.get("/logout",function(req,res){
+app.get("/logout", function(req, res) {
   req.logout();
   res.redirect("/");
-})
+});
 
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   } else {
-    res.redirect("/");
+    res.redirect("/home");
   }
 }
 
+// edit account
+app.put("/edit/:id", function(req, res) {
+  User.findByIdAndUpdate(req.params.id, req.body.edit, function(err, updated) {
+    if (err) {
+      res.redirect("/");
+    } else {
+      res.redirect("/");
+    }
+  });
+});
 
-var port = process.env.PORT || 5000;
+// delete account
+app.delete("/del/:id", function(req, res) {
+  User.findByIdAndRemove(req.params.id, function(err) {
+    if (err) {
+      res.redirect("/home");
+    } else {
+      res.redirect("/home");
+    }
+  });
+});
 
 app.listen(port, function() {
   console.log("SERVER STARTED AT PORT 5000");
--- models/user.js
+++ models/user.js
@@ -1,14 +1,16 @@
-var  mongoose             = require("mongoose"),
-     passportLocalMongoose = require("passport-local-mongoose");
+var mongoose = require("mongoose"),
+  passportLocalMongoose = require("passport-local-mongoose");
 
 var UserSchema = new mongoose.Schema({
-        username:String,
-        firstname:String,
-        lastname:String,
-        code:String,
-        password:String
+  username: String,
+  firstname: String,
+  lastname: String,
+  gender: String,
+  mobile: String,
+  address: String,
+  password: String
 });
 
 UserSchema.plugin(passportLocalMongoose);
 
-module.exports = mongoose.model("User",UserSchema);
+module.exports = mongoose.model("User", UserSchema);
